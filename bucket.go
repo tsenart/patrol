cea9ee413e2d1abf744959c763f07485f95571aa
Tomás Senart <tsenart@gmail.com>
2018-10-18 14:36:30 +0200

    Token bucket implementation

---
+++ bucket.go
@@ -0,0 +1,138 @@
+package patrol
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+// Bucket implements a simple Token Bucket with underlying
+// CRDT G-Counter semantics which allow it to be merged without
+// coordination with other Buckets. It is safe for concurrent use.
+type Bucket struct {
+	added uint64
+	taken uint64
+	last  int64 // Unix nanoseconds timestamp since epoch
+}
+
+// Rate defines the maximum frequency of some events.
+// Rate is represented as number of events per unit of time.
+// A zero Rate allows no events.
+type Rate struct {
+	Freq int
+	Per  time.Duration
+}
+
+// IsZero returns true if either Freq or Per are zero valued.
+func (r Rate) IsZero() bool {
+	return r.Freq == 0 || r.Per == 0
+}
+
+// Duration is a unit conversion function from the number of tokens to the duration
+// of time it takes to accumulate them at the given rate.
+func (r Rate) Duration(tokens uint64) time.Duration {
+	if r.IsZero() {
+		return time.Duration(math.MaxInt64)
+	}
+	interval := uint64(r.Per.Nanoseconds() / int64(r.Freq))
+	return time.Duration(tokens * interval)
+}
+
+// Tokens is a unit conversion function from a time duration to the number of tokens
+// which could be accumulated during that duration at the given rate.
+func (r Rate) Tokens(d time.Duration) uint64 {
+	if r.IsZero() {
+		return 0
+	}
+
+	interval := r.Interval()
+	if interval == 0 {
+		return 0
+	}
+
+	return uint64(d / interval)
+}
+
+// Interval returns the Rate's interval between events.
+func (r Rate) Interval() time.Duration {
+	return r.Per / time.Duration(r.Freq)
+}
+
+// NewBucket returns a new Bucket with the given pre-filled tokens.
+func NewBucket(tokens uint64) *Bucket {
+	return &Bucket{added: tokens}
+}
+
+// Take attempts to take n tokens out of the Bucket with the given capacity and
+// filling Rate at time t.
+func (b *Bucket) Take(t time.Time, r Rate, n uint64) (ok bool, rem uint64) {
+	// pre and post conditions:
+	//    b.added >= b.taken
+	//    b.added - b.taken <= capacity
+
+	last, now := b.last, t.UnixNano()
+	if now < last {
+		last = now
+	}
+
+	capacity := uint64(r.Freq)
+
+	// Calculate the current number of tokens.
+	tokens := b.added - b.taken
+
+	// Avoid making delta overflow below when last is very old.
+	maxElapsed := r.Duration(capacity - tokens)
+	elapsed := time.Duration(now - last)
+	if elapsed > maxElapsed {
+		elapsed = maxElapsed
+	}
+
+	// Calculate the added number of tokens due to elapsed time.
+	added := r.Tokens(elapsed)
+
+	// New number of tokens, capped at the bucket capacity.
+	newTokens := tokens + added
+	if newTokens > capacity {
+		excess := newTokens - capacity
+		added -= excess
+		newTokens = capacity
+	}
+
+	// Number of taken tokens, capped at newTokens.
+	taken := n
+	if ok = taken <= newTokens; !ok {
+		taken = newTokens
+	}
+
+	b.last = now
+	b.added += added
+	b.taken += taken
+
+	fmt.Printf("last: %d, elapsed: %s, added: %d, taken: %d\n", b.last, elapsed, b.added, b.taken)
+
+	return ok, b.added - b.taken
+}
+
+// Merge merges multiple Buckets, using G-counter CRDT semantics
+// with its counters, picking the largest values for each field.
+func Merge(bs ...*Bucket) *Bucket {
+	var max Bucket
+	for _, b := range bs {
+		if b == nil {
+			continue
+		}
+
+		if max.added < b.added {
+			max.added = b.added
+		}
+
+		if max.taken < b.taken {
+			max.taken = b.taken
+		}
+
+		if max.last < b.last {
+			max.last = b.last
+		}
+	}
+	return &max
+}
---
+++ bucket_test.go
@@ -0,0 +1,48 @@
+package patrol
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRate(t *testing.T) {
+	r := Rate{Freq: 100, Per: time.Second}
+	d := r.Duration(1000)
+	if have, want := d, r.Duration(r.Tokens(d)); have != want {
+		t.Errorf("rate: %+v: have: %d, want: %d", r, have, want)
+	}
+}
+
+func TestBucket_Take(t *testing.T) {
+	rate := Rate{Freq: 60, Per: time.Second} // 60 tokens per second
+	interval := rate.Interval()
+	bucket := NewBucket(60)
+	now := time.Unix(0, 0)
+
+	// Test successive takes from the same bucket.
+	for _, tc := range []struct {
+		elapsed time.Duration
+		take    uint64
+		ok      bool
+		rem     uint64
+	}{
+		{elapsed: time.Millisecond, take: 1, ok: true, rem: 59},
+		{elapsed: time.Millisecond, take: 1, ok: true, rem: 58}, // no tokens added. elapsed duration is before rate interval elapsed
+		{elapsed: time.Millisecond, take: 8, ok: true, rem: 50}, // no tokens added, took 7
+		{elapsed: interval, take: 1, ok: true, rem: 50},         // add 1, take 1
+		{elapsed: interval, take: 50, ok: true, rem: 1},         // add 1, take 50
+		{elapsed: time.Millisecond, take: 2, ok: false, rem: 0}, // not enough tokens
+		{elapsed: time.Millisecond, take: 1, ok: false, rem: 0}, // empty bucket, no tokens taken
+		{elapsed: time.Second / 2, take: 0, ok: true, rem: 30},  // 30 tokens replenished
+		{elapsed: 5 * time.Minute, take: 0, ok: true, rem: 60},  // all tokens replenished, but capped at capacity
+	} {
+		now = now.Add(tc.elapsed)
+		ok, rem := bucket.Take(now, rate, tc.take)
+		if ok != tc.ok || rem != tc.rem {
+			t.Errorf(
+				"\nBucket%+v:\n\tTake elapsed: %s, rate: %v, n: %d\n\t\thave (%t, %d)\n\t\twant (%t, %d)",
+				bucket, tc.elapsed, rate, tc.take, ok, rem, tc.ok, tc.rem,
+			)
+		}
+	}
+}
